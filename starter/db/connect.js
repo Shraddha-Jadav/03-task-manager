d4f29abaa0b0d3fae0abe764b47c2642574d0ce0
Shraddha Jadav <shraddha.j@shaligraminfotech.com>
2024-04-04 14:27:59 +0530

    CRUD operation complete

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectionString =
-    "mongodb+srv://shraddhajadav635:shraddha123@nodeexpressprojects.vzjh08s.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects"
- 
 const connectDB = (url) => {
     console.log("Connected...")
-    return mongoose.connect(connectionString)
+    return mongoose.connect(url)
 }
 
 module.exports = connectDB

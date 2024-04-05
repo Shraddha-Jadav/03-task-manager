7875e92fa0cd2409df45f3606a93a0f250e0549d
Shraddha Jadav <shraddha.j@shaligraminfotech.com>
2024-04-05 17:16:09 +0530

    project complete

--- starter/app.js
+++ starter/app.js
@@ -3,6 +3,8 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect');
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
 
 // middleware
 app.use(express.static('./public'))
@@ -11,11 +13,14 @@ app.use(express.json())
 // routes
 app.use('/api/v1/tasks', tasks)
 
-const port = 3000;
+app.use(notFound)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI);
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,console.log(`Running on port no ${port}`))
     } catch (error) {
         console.log(error)
--- starter/routes/tasks.js
+++ starter/routes/tasks.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controller/tasks')
+const {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+} = require('../controller/tasks')
 
 router.route('/').get(getAllTasks).post(createTask);
 router
